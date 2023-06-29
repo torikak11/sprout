8e2f54649ab37b85ee9f5a82ea41b3ee33d1fac2
Victoria Lizzi <45150448+torikak11@users.noreply.github.com>
2023-06-29 11:49:23 +0100

    updated sprout api url

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://ec2-34-216-73-169.us-west-2.compute.amazonaws.com:3000/api/v1";
+export const BASE_URL = "http://ec2-34-210-141-228.us-west-2.compute.amazonaws.com:3000/api/v1";
